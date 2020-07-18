437e367d30a6570c719f5e33821a77f6d11371dc
HenryBlairG <henry.blair.g@gmail.com>
2020-07-18 17:39:55 -0400

    18/07/2020 - minor changes

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 """
 Main File for Program
 """
-# from libs.url_utils import send_msg_url as s_url
+from libs.url_utils import send_msg_url as s_url
 from libs.wsp import WebWSPHandler as Wsp
 from os.path import join as os_join
 from os import getcwd
 
 
+def sing_msg_multi_num(numb_path,
+                       msg_path):
+    with open(numb_path) as numbers:
+        with open(msg_path) as msg_chunks:
+            msg = msg_chunks.readlines()
+            m = "\n".join(msg)
+            browser = Wsp(
+                driver_path=os_join(
+                    getcwd(),
+                    'chromedriver'
+                )
+            )
+            browser.pressEnter(
+                [
+                    s_url(
+                        n,
+                        m
+                    ) for n in numbers
+                ]
+            )
+
+
 if __name__ == '__main__':
-    browser = Wsp(driver_path=os_join(getcwd(), 'chromedriver'))
-    browser.pressEnter([])
+    PHONE_PATH = ''
+    MSG_PATH = ''
+    sing_msg_multi_num(PHONE_PATH, MSG_PATH)
