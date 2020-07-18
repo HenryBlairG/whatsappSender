dd32f6e341198d70bc017ae2af2320a106528394
HenryBlairG <henry.blair.g@gmail.com>
2020-07-18 18:45:03 -0400

    18/07/2020 - minor changes

---
+++ tests.py
@@ -0,0 +1,12 @@
+"""
+Test file for dependencies
+"""
+import libs.url_utils as url
+
+
+def test_no_msg_url():
+    assert url.no_msg_url('123') == 'https://web.whatsapp.com/send?phone=123&text=&source&data&app_absent'
+
+
+def test_send():
+    assert url.send_msg_url('123', 'abc') == 'https://web.whatsapp.com/send?phone=123&text=abc&source&data&app_absent'
